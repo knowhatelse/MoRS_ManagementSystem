e527f3b04ddd9005210e475fd8c51be03f53be40
knowhatelse <kenan.kajtazovic.01@gmail.com>
2025-04-06 01:17:20 +0200

    Chore (API-Infrastructure): Cleanup Program file by moving code to separate extensions

--- src/api/MoRS.ManagementSystem.API/Program.cs
+++ src/api/MoRS.ManagementSystem.API/Program.cs
@@ -1,47 +1,17 @@
-using MoRS.ManagementSystem.Infrastructure.Data;
-using Scalar.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using MoRS.ManagementSystem.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-
 var connectionString = builder.Configuration.GetConnectionString("MoRSManagementSystemDatabase");
 
-// Add services to the container.
-builder.Services
-    .AddMoRSManagementSystemDbContext(connectionString!)
-    .AddDataSeeder()
-    .AddRepositories();
-
+builder.Services.AddMoRSManagementSystemDbContext(connectionString!);
+builder.Services.AddDataSeeder();
+builder.Services.AddRepositories();
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Seed the database with test data.
-using (var scope = app.Services.CreateScope())
-{
-    var seeder = scope.ServiceProvider.GetRequiredService<DataSeeder>();
-    seeder.SeedData().Wait();
-}
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.MapScalarApiReference(options =>
-    {
-        options
-            .WithTitle("MoRS Management System API")
-            .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
-    });
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+app.SeedDatabaseAsync().Wait();
+app.ConfigurePipeline();
 app.Run();
