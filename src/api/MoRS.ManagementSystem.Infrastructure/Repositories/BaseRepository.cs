4bcc371fcb08ec8d8585c11be464d6377a83e821
knowhatelse <kenan.kajtazovic.01@gmail.com>
2025-04-06 10:55:17 +0200

    Refactor (Infrastructure): Declare base repository methods as virtual

--- src/api/MoRS.ManagementSystem.Infrastructure/Repositories/BaseRepository.cs
+++ src/api/MoRS.ManagementSystem.Infrastructure/Repositories/BaseRepository.cs
@@ -9,31 +9,31 @@ public class BaseRepository<TEntity>(MoRSManagementSystemDbContext context) : IB
 {
     private readonly MoRSManagementSystemDbContext _context = context;
 
-    public async Task<TEntity> AddAsync(TEntity entity)
+    public virtual async Task<TEntity> AddAsync(TEntity entity)
     {
         await _context.Set<TEntity>().AddAsync(entity);
         await _context.SaveChangesAsync();
         return entity;
     }
 
-    public async Task<bool> DeleteAsync(TEntity entity)
+    public virtual async Task<bool> DeleteAsync(TEntity entity)
     {
         _context.Set<TEntity>().Remove(entity);
         var result = await _context.SaveChangesAsync();
         return result > 0;
     }
 
-    public async Task<IEnumerable<TEntity>> GetAsync()
+    public virtual async Task<IEnumerable<TEntity>> GetAsync()
     {
         return await _context.Set<TEntity>().ToListAsync();
     }
 
-    public async Task<TEntity?> GetByIdAsync(int id)
+    public virtual async Task<TEntity?> GetByIdAsync(int id)
     {
         return await _context.Set<TEntity>().FindAsync(id);
     }
 
-    public async Task<TEntity> UpdateAsync(TEntity entity)
+    public virtual async Task<TEntity> UpdateAsync(TEntity entity)
     {
         _context.Set<TEntity>().Update(entity);
         await _context.SaveChangesAsync();
