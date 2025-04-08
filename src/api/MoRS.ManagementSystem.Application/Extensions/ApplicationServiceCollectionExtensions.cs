f809d2e17cdd1f6452735fc0ea41ce7eb1e9ee5b
knowhatelse <kenan.kajtazovic.01@gmail.com>
2025-04-08 16:10:42 +0200

    Feat (Application): Register services for dependency injection

---
+++ src/api/MoRS.ManagementSystem.Application/Extensions/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+using Microsoft.Extensions.DependencyInjection;
+using MoRS.ManagementSystem.Application.Interfaces;
+using MoRS.ManagementSystem.Application.Services;
+
+namespace MoRS.ManagementSystem.Application.Extensions;
+
+public static class ApplicationServiceCollectionExtensions
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<IAnnouncementService, AnnouncementService>();
+        services.AddScoped<IAppointmentService, AppointmentService>();
+        services.AddScoped<IEmailService, EmailService>();
+        services.AddScoped<IMalfunctionReportService, MalfunctionReportService>();
+        services.AddScoped<IProfilePictureService, ProfilePictureService>();
+        services.AddScoped<IRoomService, RoomService>();
+        services.AddScoped<IUserService, UserService>();
+
+        return services;
+    }
+}
