0b1bf4e9aa5c186677e41ff7f5d6be703e637de8
knowhatelse <kenan.kajtazovic.01@gmail.com>
2025-04-27 17:02:22 +0200

    Feat (Application): Implement user login

--- src/api/MoRS.ManagementSystem.Application/Utils/ErrorMessages.cs
+++ src/api/MoRS.ManagementSystem.Application/Utils/ErrorMessages.cs
@@ -4,4 +4,5 @@ public static class ErrorMessages
 {
     public const string TimeSlotConflict = "Termin u ovoj prostoriji već postoji u odabranom vremenskom terminu";
     public const string EmailAlreadyExists = "Korisnik sa unesenom email adresom već postoji u sistemu";
+    public const string InvalidCredentials = "Nesipravana email adresa ili lozinka";
 }
