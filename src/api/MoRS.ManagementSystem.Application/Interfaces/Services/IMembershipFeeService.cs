d5901b141d9cd89a64f0e185f8dff922d0ac4b48
knowhatelse <kenan.kajtazovic.01@gmail.com>
2025-05-12 17:24:56 +0200

    Feat (Application): Add business logic for membership fee

---
+++ src/api/MoRS.ManagementSystem.Application/Interfaces/Services/IMembershipFeeService.cs
@@ -0,0 +1,10 @@
+using MoRS.ManagementSystem.Application.DTOs.MembershipFee;
+using MoRS.ManagementSystem.Application.Filters;
+using MoRS.ManagementSystem.Application.Interfaces.Services.BaseInterfaces;
+
+namespace MoRS.ManagementSystem.Application.Interfaces.Services;
+
+public interface IMembershipFeeService : IGetService<MembershipFeeResponse, NoQuery>
+{
+
+}
---
+++ src/api/MoRS.ManagementSystem.Application/Services/MembershipFeeService.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using MoRS.ManagementSystem.Application.DTOs;
+using MoRS.ManagementSystem.Application.DTOs.MembershipFee;
+using MoRS.ManagementSystem.Application.Filters;
+using MoRS.ManagementSystem.Application.Interfaces.Repositories;
+using MoRS.ManagementSystem.Application.Interfaces.Services;
+using MoRS.ManagementSystem.Domain.Entities;
+
+namespace MoRS.ManagementSystem.Application.Services;
+
+public class MembershipFeeService(IMapper mapper, IMembershipFeeRepository repository) :
+    BaseService<MembershipFee, MembershipFeeResponse, EmptyDto, EmptyDto, NoQuery>(mapper, repository),
+    IMembershipFeeService
+{
+
+}
