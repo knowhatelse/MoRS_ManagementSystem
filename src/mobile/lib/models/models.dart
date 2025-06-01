67ed08ec5f1672372c8382099d7ad57e212f7469
knowhatelse <kenan.kajtazovic.01@gmail.com>
2025-06-01 17:06:35 +0200

    Feat (Mobile): Create the request and response models for login functionality

---
+++ src/mobile/lib/models/models.dart
@@ -0,0 +1,12 @@
+// Authentication models
+export 'authentication/login_request.dart';
+export 'authentication/login_response.dart';
+
+// User models
+export 'user/user_response.dart';
+
+// Profile picture models
+export 'profile_picture/profile_picture_response.dart';
+
+// Role models
+export 'role/role_response.dart';
