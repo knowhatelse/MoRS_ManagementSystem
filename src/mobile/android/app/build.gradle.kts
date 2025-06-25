78558b683631ca161cb6e973aaa23b7e1eda4547
knowhatelse <kenan.kajtazovic.01@gmail.com>
2025-06-25 15:11:46 +0200

    Chore (Mobile): Update the Android NDK version

--- src/mobile/android/app/build.gradle.kts
+++ src/mobile/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.mors_management_system_mobile"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
